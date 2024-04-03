079be0e94e414d7ec3625a7d3a7c81ef74ebd3ed
ScrewdriverHead <ashraf.ibrahim@student.tdsb.on.ca>
2024-04-03 11:36:48 -0400

    java intro

---
+++ Java Intro/src/mainFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+//Imports javax class package
+
+public class mainFrame extends JFrame{
+    public void init(){
+        setTitle("Pop Up Window!! :):)");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation
+        (WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
