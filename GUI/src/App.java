6cbdffb443d3311e8cb164a7c7d29f147e468323
ScrewdriverHead <ashraf.ibrahim@student.tdsb.on.ca>
2024-04-16 11:22:02 -0400

    Java Swing GUI

---
+++ GUI/src/App.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = JOptionPane.showInputDialog("Enter your name ");
+        JOptionPane.showMessageDialog(null, "Hello" +name);
+        int age = Integer.parseInt(JOptionPane.showInputDialog("How old are you?"));
+        
+    }
+}
