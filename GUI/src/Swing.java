06e228d988864bea20d11215980afd3ac2152782
ScrewdriverHead <ashraf.ibrahim@student.tdsb.on.ca>
2024-04-16 11:31:28 -0400

    swing addition

--- GUI/src/Swing.java
+++ GUI/src/Swing.java
@@ -3,8 +3,8 @@ import javax.swing.JOptionPane;
 public class Swing {
     public static void main(String[] args) throws Exception {
         String name = JOptionPane.showInputDialog("Enter your name ");
-        JOptionPane.showMessageDialog(null, "Hello" +name);
+        JOptionPane.showMessageDialog(null, "Hello " + name);
         int age = Integer.parseInt(JOptionPane.showInputDialog("How old are you?"));
-        JOptionPane.showMessageDialog(null, name);
+        JOptionPane.showMessageDialog(null, "You are "+ age + " years old");
     }
 }
