64b861e5f72458ba28a1b3ffe9bf82d5df754a38
ScrewdriverHead <ashraf.ibrahim@student.tdsb.on.ca>
2024-04-16 11:28:24 -0400

    Java Project fix

--- GUI/src/Swing.java
+++ GUI/src/Swing.java
@@ -1,10 +1,10 @@
 import javax.swing.JOptionPane;
 
-public class App {
+public class Swing {
     public static void main(String[] args) throws Exception {
         String name = JOptionPane.showInputDialog("Enter your name ");
         JOptionPane.showMessageDialog(null, "Hello" +name);
         int age = Integer.parseInt(JOptionPane.showInputDialog("How old are you?"));
-        
+        JOptionPane.showMessageDialog(null, name);
     }
 }
