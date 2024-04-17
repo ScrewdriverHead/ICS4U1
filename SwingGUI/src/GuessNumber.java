e2e4ab108dc19e890d7309c5f097c52e204983d3
Ashraf <ashraf.ibrahim@student.tdsb.on.ca>
2024-04-17 19:53:48 -0400

    Swing GUI progress 2

--- SwingGUI/src/GuessNumber.java
+++ SwingGUI/src/GuessNumber.java
@@ -6,12 +6,14 @@ public class GuessNumber {
         int loop = 1;
 
         JOptionPane.showMessageDialog(null, "I am thinking of a number between 1 and 100, guess what it is.");
-        
+
         while(loop == 1){
             int guess  = Integer.parseInt(JOptionPane.showInputDialog(""));
+            
             if(guess>num){
                 JOptionPane.showMessageDialog(null, "That number is too high.");
             }
+
             else if(guess<num){
                 JOptionPane.showMessageDialog(null, "That number is too low.");
             }
