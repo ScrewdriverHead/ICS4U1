52d0560970e867ad5466b7c96103f44e116f39f6
Ashraf <ashraf.ibrahim@student.tdsb.on.ca>
2024-04-17 17:47:34 -0400

    Swing GUI Progress

---
+++ SwingGUI/src/GuessNumber.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class GuessNumber {
+    public static void main(String[] args) throws Exception {
+        int num = 37;
+        int loop = 1;
+
+        JOptionPane.showMessageDialog(null, "I am thinking of a number between 1 and 100, guess what it is.");
+        
+        while(loop == 1){
+            int guess  = Integer.parseInt(JOptionPane.showInputDialog(""));
+            if(guess>num){
+                JOptionPane.showMessageDialog(null, "That number is too high.");
+            }
+            else if(guess<num){
+                JOptionPane.showMessageDialog(null, "That number is too low.");
+            }
+
+            else if(guess == num){
+                JOptionPane.showMessageDialog(null, "Congratulations, correct number.");
+                loop = 0;
+            }
+        }
+        }
+        
+    }
---
+++ SwingGUI/src/TrueFalse.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class TrueFalse {
+    public static void main(String[] args) throws Exception {
+        int tf = 1;
+        while (tf == 1)
+            {
+                tf = JOptionPane.showConfirmDialog(null, "Is the sky blue?", "A Question", JOptionPane.YES_NO_OPTION);
+                if (tf == 1) 
+                    {
+                        JOptionPane.showMessageDialog(null,"Sorry, wrong answer. Try again.", "Wrong", JOptionPane.INFORMATION_MESSAGE);
+                    }
+
+                else if (tf == 0) 
+                    {
+                JOptionPane.showMessageDialog(null, "Correct answer. Good job!", "Correct", JOptionPane.INFORMATION_MESSAGE);
+                System.exit(0);
+                    }
+            }
+
+        }
+    }
